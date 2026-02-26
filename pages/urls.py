f1d2931cdf77312b5d13235725fbb8bb98c12f91
Sara Echeverri <egsarita@gmail.com>
2026-02-26 16:36:40 -0500

    feat(carrito): agregar carrito y actualizar plantillas

--- pages/urls.py
+++ pages/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from .views import (
+    CartView,
     HomePageView,
     AboutPageView,
     ProductIndexView,
     ProductShowView,
     ProductCreateView,
-    ContactPageView
+    ContactPageView,
+    CartView, 
+    CartRemoveAllView
 )
-
+ 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
@@ -17,4 +20,7 @@ urlpatterns = [
     path('products/create', ProductCreateView.as_view(), name='form'),
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
     path('contact/', ContactPageView.as_view(), name='contact'),
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
 ]
