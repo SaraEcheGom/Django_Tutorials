69eab4a55f565b8466317d9de1d5ecbc4e23bc6f
Sara Echeverri <egsarita@gmail.com>
2026-03-06 16:22:20 -0500

    feat(backend): update django project

--- helloworld_project/settings.py
+++ helloworld_project/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
+    'rest_framework',
+    'todoapp',
 ]
 
 MIDDLEWARE = [
--- helloworld_project/urls.py
+++ helloworld_project/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('api/', include('todoapp.backend.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
